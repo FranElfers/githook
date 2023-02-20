51ba676946eae401ff859bfa83cf049d35486184
Francael <fransozzan@gmail.com>
2023-02-19 22:36:45 -0300

    permitir todo

--- index.js
+++ index.js
@@ -1,24 +1,35 @@
+// https://www.digitalocean.com/community/tutorials/how-to-use-node-js-and-github-webhooks-to-keep-remote-projects-in-sync
+
 const crypto = require('crypto')
 const express = require('express')
 const app = express()
+const exec = require('child_process').exec
+
+const secret = ''
 
 app.get('/', (req, res) => {
 	res.send('Hey')
 })
 
 app.post('/', (req, res) => {
-	// desencriptar secret de github
-	const sig = "sha1=" + crypto.createHmac('sha1', secret).update(chunk.toString()).digest('hex')
+	req.on('data', (chunk) => {
+		// desencriptar secret de github
+		const sig = "sha1=" + crypto.createHmac('sha1', secret).update(chunk.toString()).digest('hex')
+	
+		const isAuthentic = req.headers['x-hub-signature'] === sig
+	
+		if (!isAuthentic) return res.send('Not authorized')
+	
+		exec('sh pipe.sh')
+	
+		res.send('Got it')
+	})
 
-	const isAuthentic = req.headers['x-hub-signature'] === sig
+	res.end()
+})
 
-	if (!isAuthentic) {
-		return res.send('Not authorized')
-	}
+app.post('/test', (req, res) => {
 
-	exec('sh pipe.sh')
-
-	res.send('Got it')
 })
 
 app.listen(3002, () => console.log('listening on 3002'))
